0c69716e117b34b1062e4fe4cad6f626c2b2acb1
Aziz Amerul Faozi <admin@labseni.com>
2019-05-11 02:22:42 +0700

    udah work lah

---
+++ src/main/java/com/labseni/cilukba/UserRepository.java
@@ -0,0 +1,12 @@
+package com.labseni.cilukba;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.labseni.cilukba.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}

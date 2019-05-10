8bdb4e598c1fbfefee853643fa28ee87bf71ff7e
Aziz Amerul Faozi <admin@labseni.com>
2019-05-11 01:59:32 +0700

    freak

---
+++ src/main/java/com/labseni/cilukba/CilukbaApplication.java
@@ -0,0 +1,13 @@
+package com.labseni.cilukba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CilukbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CilukbaApplication.class, args);
+	}
+
+}
